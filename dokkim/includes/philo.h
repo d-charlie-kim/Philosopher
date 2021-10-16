3fdeabbf8749088ce53c40fc93ad2e3a6e3f1fa4
charlie com <dokkim@student.42seoul.kr>
2021-10-16 14:14:26 +0900

    start

---
+++ dokkim/includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dokkim <dokkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/15 21:20:16 by dokkim            #+#    #+#             */
+/*   Updated: 2021/10/15 21:22:56 by dokkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+#endif
