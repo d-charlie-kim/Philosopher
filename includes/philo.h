c822ca74fac54902366569059b32a0042a5f8963
Dokyeong Kim <dokkim@c10r2s5.42seoul.kr>
2021-10-17 18:32:25 +0900

    making struct

--- includes/philo.h
+++ includes/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dokkim <dokkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: dokkim <dokkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 21:20:16 by dokkim            #+#    #+#             */
-/*   Updated: 2021/10/15 21:22:56 by dokkim           ###   ########.fr       */
+/*   Updated: 2021/10/17 18:28:49 by dokkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,32 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+
+typedef struct s_info
+{
+	long long	philos_num;
+	long long	time_to_die;
+	long long	time_to_eat;
+	long long	time_to_sleep;
+	long long	max_eat;
+}	t_info;
+
+typedef struct s_philo
+{
+	long long	philo_num;
+	long long	right_fork;
+	long long	left_fork;
+	long long	num_ate;
+	long long	status;
+}	t_philo;
+
+
+
+typedef struct s_system
+{
+	t_info	info;
+	t_philo	philos;
+	...
+}	t_system;
 
 #endif
